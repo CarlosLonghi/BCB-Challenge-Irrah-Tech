080088135beb70ee34b82fe835d27e3129f9c1fd
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-05-08 10:52:06 -0300

    Configuracao inicial: adiciona setup inicial do projeto

---
+++ src/main/java/com/carloslonghi/bcb/BcbApplication.java
@@ -0,0 +1,13 @@
+package com.carloslonghi.bcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcbApplication.class, args);
+	}
+
+}
