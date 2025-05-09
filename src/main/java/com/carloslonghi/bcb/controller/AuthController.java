e8e48ea92083e0da84ed1681a9bbb3f218ab15f2
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-05-09 11:19:18 -0300

    feat: adiciona e implementa `Spring Security` para autenticacao

---
+++ src/main/java/com/carloslonghi/bcb/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.carloslonghi.bcb.controller;
+
+import com.carloslonghi.bcb.dto.AuthRequest;
+import com.carloslonghi.bcb.dto.AuthResponse;
+import com.carloslonghi.bcb.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
