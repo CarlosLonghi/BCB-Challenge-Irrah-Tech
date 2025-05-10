9132026da4a37e2d22f9c54ef9c56f6bbf5addf8
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-05-10 16:17:28 -0300

    feat: adicona metodos em `Conversation` e `Message` repository

--- src/main/java/com/carloslonghi/bcb/repository/MessageRepository.java
+++ src/main/java/com/carloslonghi/bcb/repository/MessageRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findBySenderId(Long senderId);
+    List<Message> findByConversationId(Long conversationId);
 }
