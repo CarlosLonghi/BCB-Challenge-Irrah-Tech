5f849713809a3d52b0297d9e6ec951b359641bd2
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-05-10 23:06:27 -0300

    feat: adiciona metodo `countByStatus` em `Message` repository

--- src/main/java/com/carloslonghi/bcb/repository/MessageRepository.java
+++ src/main/java/com/carloslonghi/bcb/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.carloslonghi.bcb.repository;
 
 import com.carloslonghi.bcb.model.Message;
+import com.carloslonghi.bcb.model.enums.MessageStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.List;
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findBySenderId(Long senderId);
     List<Message> findByConversationId(Long conversationId);
+    int countByStatus(MessageStatus status);
 }
